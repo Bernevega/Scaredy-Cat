4fe5dbfe9594795714e256b7f4b975f7ab2bcc13
Socolofii <pseudotropheus1@gmail.com>
2025-06-09 16:18:40 +0300

    Crow prototype

--- Assets/Scripts/Crow/Crow.cs
+++ Assets/Scripts/Crow/Crow.cs
@@ -3,14 +3,33 @@ using UnityEngine;
 public class Crow : MonoBehaviour
 {
     [SerializeField] Interactable interactable;
-    [SerializeField] ItemScriptable monocleScriptable;
+    [SerializeField] ItemScriptable monocleLeftScriptable;
+    [SerializeField] ItemScriptable monocleRightScriptable;
 
-    [SerializeField] GameObject monocle1Object;
+    [Space(25)]
+    [SerializeField] GameObject monocleLeftObject;
+    [SerializeField] GameObject monocleRightObject;
+    [SerializeField] GameObject flowerObject;
+    [SerializeField] GameObject bushObject;
 
     Item[] itemReturnArray = new Item[1];
 
+    DialogueState dialogueState = DialogueState.FirstTalk;
+
     bool hasThanked = false;
-    bool hasMonocle = false;
+    bool hasLeftMonocle = false;
+    bool hasRightMonocle = false;
+
+    enum DialogueState
+    {
+        FirstTalk,
+        FirstMonocleWait,
+        SecondMonocle,
+        SecondMonocleWait,
+        AllMonoclesGiven,
+        Final,
+        FinalFinal
+    }
 
     private void Awake()
     {
@@ -21,33 +40,89 @@ public class Crow : MonoBehaviour
     {
         if (type == InteractActionType.Interact)
         {
-            checkMonocle(interactor);
+            if (dialogueState == DialogueState.FirstTalk)
+            {
+                StartDialogue();
+            }
+            else
+            {
+                checkMonocle(interactor);
+            }
         }
     }
 
     public void checkMonocle(Interactor interactor)
     {
-        if (!hasMonocle)
+        if (dialogueState == DialogueState.FirstMonocleWait && !hasLeftMonocle)
         {
             InventoryScript inventoryScript = interactor.GetOwner().GetComponent<InventoryScript>();
-            int numItem = inventoryScript.GetItem_WithScriptable(itemReturnArray, monocleScriptable);
+            int numItem = inventoryScript.GetItem_WithScriptable(itemReturnArray, monocleLeftScriptable);
 
             if (numItem > 0)
             {
                 inventoryScript.RemoveItem(itemReturnArray[0], -1);
                 Destroy(itemReturnArray[0]);
 
-                monocle1Object.SetActive(true);
-                hasMonocle = true;
+                monocleLeftObject.SetActive(true);
+                bushObject.SetActive(false);
+                hasLeftMonocle = true;
+
+                dialogueState = DialogueState.SecondMonocle;
             }
-            else
-            {
-                SimpleDialogManager.Instance.StartDialogue("CrowTest");
-            }
+
+            StartDialogue();
         }
-        else if (!hasThanked)
+        else if (dialogueState == DialogueState.SecondMonocleWait && !hasRightMonocle)
         {
-            SimpleDialogManager.Instance.StartDialogue("CrowThank");
+            InventoryScript inventoryScript = interactor.GetOwner().GetComponent<InventoryScript>();
+            int numItem = inventoryScript.GetItem_WithScriptable(itemReturnArray, monocleRightScriptable);
+
+            if (numItem > 0)
+            {
+                inventoryScript.RemoveItem(itemReturnArray[0], -1);
+                Destroy(itemReturnArray[0]);
+
+                monocleRightObject.SetActive(true);
+                flowerObject.SetActive(true);
+                hasRightMonocle = true;
+
+                dialogueState = DialogueState.Final;
+            }
+
+            StartDialogue();
+        }
+        else if (dialogueState == DialogueState.FinalFinal && !hasThanked)
+        {
+            StartDialogue();
+            hasThanked = true;
+        }
+    }
+
+    private void StartDialogue()
+    {
+        switch (dialogueState)
+        {
+            case DialogueState.FirstTalk:
+                dialogueState = DialogueState.FirstMonocleWait;
+                SimpleDialogManager.Instance.StartDialogue("CrowFirstTalk");
+                break;
+            case DialogueState.FirstMonocleWait:
+                SimpleDialogManager.Instance.StartDialogue("CrowFirstMonocleWait");
+                break;
+            case DialogueState.SecondMonocle:
+                dialogueState = DialogueState.SecondMonocleWait;
+                SimpleDialogManager.Instance.StartDialogue("CrowSecondMonocle");
+                break;
+            case DialogueState.SecondMonocleWait:
+                SimpleDialogManager.Instance.StartDialogue("CrowSecondMonocleWait");
+                break;
+            case DialogueState.Final:
+                dialogueState = DialogueState.FinalFinal;
+                SimpleDialogManager.Instance.StartDialogue("CrowFinal");
+                break;
+            case DialogueState.FinalFinal:
+                SimpleDialogManager.Instance.StartDialogue("CrowThank");
+                break;
         }
     }
 }
