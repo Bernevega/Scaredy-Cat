d3cc145c94559698630340fa109ccc44af4cd5d6
Anomalocarida <pseudotropheus1@gmail.com>
2025-06-13 14:35:12 +0300

    stuff

--- Assets/Scripts/Directors/OutskirtsSceneDirector.cs
+++ Assets/Scripts/Directors/OutskirtsSceneDirector.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class OutskirtsSceneDirector : MonoBehaviour
 {
     [SerializeField] GameObject ghostPaper;
+    [SerializeField] GameObject logBarrier;
 
     private void Awake()
     {
         if (Crow.hasBothMonocles)
         {
             ghostPaper.SetActive(true);
+            logBarrier.SetActive(true);
         }
     }
 }
