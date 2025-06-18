44584b5220c0727b1395298041acecc17ca5dbdb
Anomalocarida <pseudotropheus1@gmail.com>
2025-06-18 11:32:54 +0300

    OyenTest

---
+++ Assets/Scripts/Crow/Oyen.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Oyen : MonoBehaviour
+{
+    [SerializeField] Interactable interactable;
+    [SerializeField] ItemScriptable braceletItemScriptable;
+
+    Item[] itemReturnArray = new Item[1];
+
+    bool waitingForBracelet = false;
+    bool hasBracelet = false;
+
+    private void Awake()
+    {
+        interactable.eventOnInteract += OnInteract;
+    }
+
+    private void OnInteract(Interactor interactor, Interactable interactable, InteractActionType type)
+    {
+        if (type == InteractActionType.Interact)
+        {
+            CheckBracelet(interactor);
+        }
+    }
+
+    private void CheckBracelet(Interactor interactor)
+    {
+        // Block if dialog active
+        SimpleDialogManager dm = SimpleDialogManager.Instance;
+        if (dm != null && dm.dialogPanel != null && dm.dialogPanel.activeSelf)
+            return;
+
+        if (!waitingForBracelet && !hasBracelet)
+        {
+            dm.StartDialogue("OyenStart");
+            waitingForBracelet = true;
+        }
+        else if (waitingForBracelet && !hasBracelet)
+        {
+            InventoryScript invScript = interactor.GetOwner().GetComponent<InventoryScript>();
+            int numItems = invScript.GetItem_WithScriptable(itemReturnArray, braceletItemScriptable);
+
+            if (numItems > 0)
+            {
+                invScript.RemoveItem(itemReturnArray[0], -1);
+                hasBracelet = true;
+
+                dm.StartDialogue("OyenThank");
+            }
+            else
+            {
+                dm.StartDialogue("OyenWait");
+            }
+        }
+        else if (waitingForBracelet && hasBracelet)
+        {
+            dm.StartDialogue("OyenThank");
+        }
+    }
+}
